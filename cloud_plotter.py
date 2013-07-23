eeb9906dd44121910c16d145cac41776205fe1a8
yacobuk <jaygattuso.github@googlemail.com>
2013-07-23 19:45:47 +1200

    [revision] makes long and lat distances roughly accurate
    Instead of a rough approximation of distance, long and lat are calculated as a distance vector from a datum long, lat pair

--- cloud_plotter.py
+++ cloud_plotter.py
@@ -3,15 +3,78 @@ import csv
 import numpy
 import math
 
+#http://wfs-beta.geonet.org.nz/geoserver/geonet/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonet:quake&outputFormat=csv&cql_filter=BBOX%28origin_geom,174,-41,175,-42%29+AND+origintime%3E=%272013-07-18%27+AND+magnitude%3E3
+
 class Points_Maker(object):
     def __init__(self, fname):
         self.fname = fname
+        self.multiplyer = 1
+        self.latitude = 0.001
+        self.logitude = None
+        self.depth = None
+        self.earth_radius = 6373 #ref radius @ sea level (km)
+        self.long_min = 174 * self.multiplyer
+        self.long_max = 175 * self.multiplyer
+        self.lat_min = -41 * self.multiplyer
+        self.lat_max = -42 * self.multiplyer
         self.points = []
         self.points_mag = []
-        self.multiplyer = 100
-        self.max_depth = float(155)
-        self.bounds = [[0, 0, 0], [0, -self.multiplyer, 0], [self.multiplyer, 0, 0], [self.multiplyer, -self.multiplyer, 0],[0, 0, self.max_depth], [0, -self.multiplyer, self.max_depth], [self.multiplyer, 0, self.max_depth], [self.multiplyer, -self.multiplyer, self.max_depth]]
+        self.max_depth = None
+        self.bounds = None
         self.get_data()
+
+    def make_bounds(self):
+        self.bounds = [[self.lat_min, self.long_min, 0], \
+                       [self.lat_min, self.long_max, 0], \
+                       [self.lat_max, self.long_min, 0], \
+                       [self.lat_max, self.long_max, 0], \
+                       [self.lat_min, self.long_min, self.max_depth], \
+                       [self.lat_min, self.long_max, self.max_depth], \
+                       [self.lat_max, self.long_min, self.max_depth], \
+                       [self.lat_max, self.long_max, self.max_depth]]
+
+    def convert_depth_to_relative_radius(self, depth):
+        relative_radius = self.earth_radius - depth
+        return relative_radius
+
+    def distance_on_unit_sphere(self, lat1, long1, lat2, long2, depth):
+        # Convert latitude and longitude to
+        # spherical coordinates in radians.
+        degrees_to_radians = math.pi/180.0
+
+        # phi = 90 - latitude
+        phi1 = (90.0 - lat1)*degrees_to_radians
+        phi2 = (90.0 - lat2)*degrees_to_radians
+
+        # theta = longitude
+        theta1 = long1*degrees_to_radians
+        theta2 = long2*degrees_to_radians
+
+        # Compute spherical distance from spherical coordinates.
+
+        # For two locations in spherical coordinates
+        # (1, theta, phi) and (1, theta, phi)
+        # cosine( arc length ) =
+        #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+        # distance = rho * arc length
+
+        cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+               math.cos(phi1)*math.cos(phi2))
+        arc = math.acos( cos )
+        # Remember to multiply arc by the radius of the earth
+        # in your favorite set of units to get length.
+
+        relative_radius = self.convert_depth_to_relative_radius(depth)
+        distance = arc * relative_radius
+        return distance
+
+    def make_x_point(self, lat, depth):
+        distance = self.distance_on_unit_sphere(self.lat_min, self.long_min, lat, self.long_min, depth)
+        return distance
+
+    def make_y_point(self, longi, depth):
+        distance = self.distance_on_unit_sphere(self.lat_min, self.long_min, self.lat_min, longi, depth)
+        return distance
 
     def get_data(self):
         reader = csv.reader(open(self.fname, "rb"))
@@ -19,16 +82,27 @@ class Points_Maker(object):
             if "FID" in row[0]:
                 pass
             else:
-                longitude = (float(row[3]) - 174) * self.multiplyer #de-localises value
-                latitude = (float(row[4]) + 41) * self.multiplyer #de-localises value
-                depth = float(row[5])
+                self.longitude = float(row[3])
+                self.latitude = float(row[4])
+                self.depth = float(row[5])
                 magnitude = float(row[6])
-                point = [longitude, latitude, depth]
-                point_and_mag = [[longitude, latitude, depth], magnitude]
+                x_point = self.make_x_point(self.latitude, self.depth)
+                y_point = self.make_y_point(self.longitude, self.depth)
+                point = [x_point,  y_point, self.depth]
+                point_and_mag = [[x_point,  y_point, self.depth], magnitude]
                 point = numpy.asarray(point)
                 point_and_mag = numpy.asarray(point_and_mag)
                 self.points.append(point)
                 self.points_mag.append(point_and_mag)
+                if self.depth > self.max_depth:
+                    self.max_depth = self.depth * self.multiplyer
+
+    def convert_bound_to_distance(self, bound):
+        x, y, depth = bound
+        x_point = self.make_x_point(x, depth)
+        y_point = self.make_y_point(y, depth)
+        bound = (x_point, y_point, depth)
+        return bound
 
 class VtkPointCloud:
 
@@ -51,12 +125,10 @@ class VtkPointCloud:
             self.vtkCells.InsertNextCell(1)
             self.vtkCells.InsertCellPoint(pointId)
             self.vtkActor.GetProperty().SetPointSize(mag)
-            #.GetProperty().SetSize(mag)
         self.vtkCells.Modified()
         self.vtkPoints.Modified()
         self.vtkDepth.Modified()
         self.vtkActor.Modified()
-
 
     def clearPoints(self):
         self.vtkPoints = vtk.vtkPoints()
@@ -70,18 +142,22 @@ class VtkPointCloud:
 
 
 def main():
-    pm = Points_Maker("quake_data.csv")
+    pm = Points_Maker("quake.csv")
     renderer = vtk.vtkRenderer()
     pointCloud2 = VtkPointCloud()
-    for bound in pm.bounds:
-        pointCloud2.addPoint(bound, 10)
     for point_and_mag in pm.points_mag:
         mag = point_and_mag[1]
         point = point_and_mag[0]
+        print point
         if mag > 1:
             pointCloud = VtkPointCloud()
-            pointCloud.addPoint(point, math.log(mag)*1) #changes this multiplier to make the points larger
+            pointCloud.addPoint(point, math.log(mag)*1)
             renderer.AddActor(pointCloud.vtkActor)
+    pm.make_bounds()
+    for bound in pm.bounds:
+        bound = pm.convert_bound_to_distance(bound)
+        print bound
+        pointCloud2.addPoint(bound, 10)  # the number is what makes the bound markers larger
     renderer.AddActor(pointCloud2.vtkActor)
     renderer.SetBackground(.2, .3, .3)  #colour
     renderer.ResetCamera()
@@ -89,3 +165,14 @@ def main():
     # Render Window
     renderWindow = vtk.vtkRenderWindow()
     renderWindow.AddRenderer(renderer)
+
+    # Interactor
+    renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+    renderWindowInteractor.SetRenderWindow(renderWindow)
+
+    # Begin Interaction
+    renderWindow.Render()
+    renderWindowInteractor.Start()
+
+if __name__ == '__main__':
+    main()
